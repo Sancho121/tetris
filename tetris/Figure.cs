95aa9bc719b55709b8eefb80d722d6a7caf194b6
lef <you@example.com>
2022-07-01 21:54:27 +0200

    Новый класс, тест

---
+++ tetris/Figure.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tetris
+{
+    public enum FigureType
+    {
+        O,
+        J,
+        L,
+        S,
+        Z,
+        T,
+        I,
+        Point
+    }
+
+    class Figure
+    {
+
+    }
+}
